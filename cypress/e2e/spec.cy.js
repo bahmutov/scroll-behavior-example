4b6d0a68ba15b933cccb73c599abae7ec87f9681
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-06-06 11:59:37 -0400

    demo

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
-it('types into the input field', () => {
+it('types into the input field', // { scrollBehavior: 'center' },
+() => {
   cy.visit('index.html')
+  cy.get('#name').type('world')
+  cy.contains('#greeting', 'Hello, world!').should('be.visible')
 })
