05f3ac0baf7dce8d9077a9a674099deee4fca9dc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-06-06 12:48:44 -0400

    comments

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,16 @@
 /// <reference types="cypress" />
 
-it('types into the input field', // { scrollBehavior: 'center' },
-() => {
-  cy.visit('index.html')
-  cy.get('#name').type('world')
-  cy.contains('#greeting', 'world').should('be.visible')
-})
+it(
+  'types into the input field',
+  {
+    // apply scroll behavior to the entire test
+    // you can also specify the scroll behavior
+    // as an option to the cy.type command
+    scrollBehavior: false,
+  },
+  () => {
+    cy.visit('index.html')
+    cy.get('#name').type('world')
+    cy.contains('#greeting', 'world').should('be.visible')
+  },
+)
